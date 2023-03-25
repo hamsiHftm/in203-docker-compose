311fa33ecdf82be0835ad47475a5b9aa58d3d44d
hamsiHftm <hamsiga.rajaratnam@hftm.ch>
2023-03-25 11:16:57 +0000

    intial commit

---
+++ backend-app/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+const port = 3001
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+app.listen(port, () => {
+	console.log(`Example app listenting on port ${port}`)
+})
